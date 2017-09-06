bf8e8c7d2bc2332913af7f5a4e2d9f91b08d2e64
Johanna <johannabrockmeier@yahoo.de>
2017-09-06 14:51:09 +0200

    Basic Photoshop work and idea, character controls, one enemy

---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    float speed = 10;
+    float thrust = 20;
+    Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigid = GameObject.Find("sleepyGhost").GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+	}
+    private void FixedUpdate()
+    {
+        Moving();
+    }
+
+    void Moving()
+    {
+        float transition = Input.GetAxis("Horizontal") * speed;
+        //float straffe = Input.GetAxis("Vertical") * speed;  // no flying! TOO TIRED or GETTING SKILL LATER
+        transition *= Time.deltaTime;
+        //straffe *= Time.deltaTime;
+        transform.Translate(transition, /*straffe*/ 0, 0);
+    }
+}
---
+++ Assets/scripts/tongueEnemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tongueEnemy : MonoBehaviour
+{
+    float movement;
+    public float speed = 5;
+    public Transform startMarker;
+    public Transform endMarker;
+    float currentY;
+    private float startTime;
+    private float journeyLength;
+    float t;
+    public bool hastoTurn = false;
+
+    private void Start()
+    {
+        startMarker = GameObject.Find("start").transform;
+        endMarker = GameObject.Find("end").transform;
+
+        startTime = Time.time;
+        journeyLength = Vector2.Distance(startMarker.position, endMarker.position);
+
+        currentY = gameObject.transform.position.y;
+    }
+
+    private void Update()
+    {
+        enemyMovement();
+
+        if (transform.position.x == endMarker.position.x)
+        {
+            hastoTurn = false;
+        }
+
+        if (transform.position.x == startMarker.position.x)
+        {
+            hastoTurn = true;
+        }
+    }
+
+    void enemyMovement()
+    {
+        if (hastoTurn == false)
+        {
+            t += Time.deltaTime;
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            transform.position = Vector2.Lerp(endMarker.position, startMarker.position, t);
+        }
+
+        if (hastoTurn == true)
+        {
+            t -= Time.deltaTime;
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            transform.position = Vector2.Lerp(endMarker.position, startMarker.position, t);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == ("sleepyGhost"))
+        {
+            Debug.Log("hit Player");
+        }
+    }
+}
